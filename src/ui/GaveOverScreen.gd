f87b9e4a8a48d44d2b822b015aafe2d7acb409a6
EasternMouse <31562232+EasternMouse@users.noreply.github.com>
2021-11-23 18:41:22 +0400

    Adding new flavour text

--- src/ui/GaveOverScreen.gd
+++ src/ui/GaveOverScreen.gd
@@ -4,7 +4,6 @@ var flavour_text = [
 	"This is the end",
 	"Light is no more",
 	"Lost to the darkness",
-	"You was our last light",
 	"Endless darkness",
 	"You can't see a thing anymore",
 	"Alone in the dark",
@@ -13,17 +12,23 @@ var flavour_text = [
 	"Deaf to all but the dark",
 	"Nothing to see now",
 	"Welcome to Shadow Realm",
+	"The sun sets, everything ends",
+	"From now on, the power of darkness",
+	"Sadness, loneliness, pain, despair",
+	"Throw your hope away",
+	"This is the true freedom, isn't it",
+	"Now again, something disappears",
 ]
 
 
 func _ready() -> void:
 	var random_text = flavour_text[randi()%flavour_text.size()]
-	$VBoxContainer/RichTextLabel.bbcode_text = "[center][shake rate=20 level=5]" + random_text + "[/shake][/center]"
+	$MarginContainer/VBoxContainer/RichTextLabel.bbcode_text = "[center][shake rate=20 level=5]" + random_text + "[/shake][/center]"
 	var time = SaveLoad.time
 	var minutes = time/60
 	var seconds = time%60
 	var time_string = String("%02d" % minutes) + ":" + String("%02d" % seconds)
-	$VBoxContainer/LabelTime.text = "Your time: " + time_string
+	$MarginContainer/VBoxContainer/LabelTime.text = "Your time: " + time_string
 
 
 func _on_Button_pressed() -> void:
