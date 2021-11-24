29b8a59c756a9cc406a0946a5da21f2d341f4b28
EasternMouse <31562232+EasternMouse@users.noreply.github.com>
2021-11-24 09:17:45 +0400

    Alt+Enter to fullscreen

--- src/autoload/SaveLoad.gd
+++ src/autoload/SaveLoad.gd
@@ -17,6 +17,10 @@ func _ready() -> void:
 	transition_in_progress = false
 
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("fullscreen"):
+		OS.window_fullscreen = not OS.window_fullscreen
+
 func open_scene(new_scene: String):
 	if transition_in_progress:
 		return
